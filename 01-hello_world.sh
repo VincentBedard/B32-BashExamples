19eb0dd932d6c2f09248605fe609c628a2b891b6
Vincent Bedard <vincbedard@gmail.com>
2018-09-27 14:07:05 -0400

    finished hello word file

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+# je suis un commentaire!
+
+echo "salut!"
+
+#exit
+#echo "on ne me verra jamais!"
+
+#-n = Ne pas faire de retour de chariot
+echo -n "Vive "
+echo "Linux!"
+
+echo -e "1\n2\n3\n"
+# ctrl-o pour sauvegarder sans quitter
+
+read -p "Quel âge avez-vous : " age
+echo $age "ans! Vous paraissez plus jeune!"
+
+
+
