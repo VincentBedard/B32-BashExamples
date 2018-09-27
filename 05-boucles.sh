6e632359fd381ac6a759f967758bf72351a18a1c
Vincent Bedard <vincbedard@gmail.com>
2018-09-27 14:56:59 -0400

    Finished Boucle

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+	echo $i
+done
+
+liste=`ls` #Accent grave permet d,exécuter uen commande
+
+
+for element in $liste
+do
+	echo $element
+done
+
+user=`whoami`
+echo "Salut $user"
+
+resultat=o
+
+#-o = OR, -a = AND
+while test $resultat = o -o $resultat = a;do
+	read -p "Entrez une lettre : " resultat
+done
