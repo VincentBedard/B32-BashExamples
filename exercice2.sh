c3608f727abe1224badaeef10816c845910f360a
Vincent Bedard <vincbedard@gmail.com>
2018-09-27 16:54:10 -0400

    Finished

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+afficherInfo() {
+	echo "---------------------"
+	echo "d = dictionnaire "
+	echo "q = quitter "
+	echo "---------------------"
+}
+afficherInfo
+lettre=z
+while test $lettre != q -a $lettre != d;do
+	read -p "Écrire une Lettre : " lettre
+done
+if test $lettre = d;then
+	./exercice1.sh salut
+fi
--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+addSpace() {
+	for ((i=0;i<$1;i++)) 
+	do
+		echo -n "     "
+	done
+}
+Space=" "
+for ((i=0;i<10;i++)) {
+	clear
+	addSpace $i
+	echo -n "$Space @";sleep 1
+}
+clear
+addSpace 10
+echo -n "BOOM"
+sleep 0.5
+clear
