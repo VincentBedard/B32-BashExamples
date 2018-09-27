a57ed26613b8f51b35a1919eaef91f05d6ca2188
Vincent Bedard <vincbedard@gmail.com>
2018-09-27 15:21:44 -0400

    Finished

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+mafonction() {
+	echo "parametre : $1"
+}
+
+mafonction allo
+afficherInfo() {
+	echo "---------------------"
+	echo "Kernel : " `uname -rs`
+	echo "---------------------"
+}
+
+afficherInfo
